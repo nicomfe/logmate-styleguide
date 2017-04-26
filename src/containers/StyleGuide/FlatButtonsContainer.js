484f149feff64b2fcad8d070b1c59f78861e2792
Nicolas Fetter <nicofetter@gmail.com>
2017-04-27 10:49:24 +1200

    adding source code to flat buttons

--- src/containers/StyleGuide/FlatButtonsContainer.js
+++ src/containers/StyleGuide/FlatButtonsContainer.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import jsxToString from 'jsx-to-string'
 
 import StyleGuideContent from '../../components/styleGuide/StyleGuideContent'
 import { IconExitButton } from '../../components/buttons'
@@ -7,16 +8,30 @@ const FlatButtonsContainer = () => {
   return (
     <StyleGuideContent
       title="Flat buttons"
-      description={<small>
-        Flat with icons (sign out button, etc). List with all the icons in
-        here <a href="https://material.io/icons/" target="_blank" rel="noopener noreferrer">
-          Google Icons
-        </a>
-      </small>}
+      description={<div>
+        <small>
+          Flat with icons (sign out button, etc). List with all the icons in
+          here <a href="https://material.io/icons/" target="_blank" rel="noopener noreferrer">
+            Google Icons
+          </a>
+        </small>
+        <br />
+        <small>
+          {'All custom flat buttons are created in "components/buttons/index.js"'}
+        </small>
+      </div>}
+      sourceCode={sourceCode}
     >
       <IconExitButton label="Sign out" />
     </StyleGuideContent>
   )
 }
+
+const sourceCode = `import { IconExitButton } from '/components/buttons'\n
+${jsxToString(
+  <div>
+    <IconExitButton label="Sign out" />
+  </div>
+)}`
 
 export default FlatButtonsContainer
